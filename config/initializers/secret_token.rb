074cbfa26bbc044b96daaf2c98622837cfb8ddc6
Trent Ellingsen <trent.ellingsen@mindbodyonline.com>
2013-05-07 04:38:03 -0700

    initial commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gents::Application.config.secret_token = 'a5c714817627ca33697a008b416b1317b8ce8abb8d083b8481072a1a851e355eac420f8e0b238d9396935073e928ca028402758a60e68fab29cf9eee855c549b'
